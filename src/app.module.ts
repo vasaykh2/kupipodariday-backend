1003a9b048c469338ac61262363d80477b0a7b2f
Vadim Savinov <vasaykh@yandex.ru>
2023-08-14 22:05:41 +0500

    feat: сгенерируй каркас API с пом. Nest CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { OffersModule } from './offers/offers.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { WishesModule } from './wishes/wishes.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
